5c424eeba81133bec9501832c039303826deb5fe
yaesung sho <147963792+inandoutcheeseurger@users.noreply.github.com>
2025-08-19 13:17:07 +0000

    fixed error (null pointer)

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,4 @@
-import { auth, db } from "firebase-config.js";
+import { auth, db } from "./firebase-config.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 
