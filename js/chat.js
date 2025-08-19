b7db0eaac61483ac0f5c8549b01e90545c9ca5a3
yaesung sho <147963792+inandoutcheeseurger@users.noreply.github.com>
2025-08-19 13:15:59 +0000

    changed few

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,5 @@
-import { auth, db } from "./firebase-config.js";
-import { onAuthStateChanged, signOut } 
-  from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { auth, db } from "firebase-config.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 
 let currentUser = null;
